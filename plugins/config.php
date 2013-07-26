b5e18076639aa96e9830f96cb62f1ac2984e86a8
Austin <afathman@mit.edu>
2013-07-25 21:34:15 -0400

    Twitter integration=true

--- plugins/config.php
+++ plugins/config.php
@@ -11,19 +11,19 @@ $your_email_address = 'your_email_address';
  ---------------------------------------------------------------------- */
 
 // Twitter account name
-$username = '';
+$username = 'Maseeh_Hall';
 
 // Twitter consumer key
-$consumer_key = '';
+$consumer_key = 'Fr1n0UfyMenHPkn9TvbgpA';
 
 // Twitter consumer secret key
-$consumer_secret = '';
+$consumer_secret = 'TfK3Ll7kjkAICYh9SrCvcs8PuogauKfPx1TjDSjzb9I';
 
 // Twitter access token
-$access_token = '';
+$access_token = '1602373820-1gFGFhkOwVZ1QUAtZUZWB9bj3kQKPcijxgGovAq';
 
 // Twitter access token secret
-$access_token_secret = '';
+$access_token_secret = '2r8oR88xcwz04STZbIRaCpNAAQP3DiKBCdS37rRewEQ';
 
 // Do you want to show replies
 $ignore_replies = true;
